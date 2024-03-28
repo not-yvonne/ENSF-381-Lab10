e365956153ed5a5072cad698052daffe48956c2c
JeeyaSomaiya <144636268+JeeyaSomaiya@users.noreply.github.com>
2024-03-27 22:19:09 -0600

    Updated server.py for exercise B

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask , request , jsonify , send_from_directory
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS ( app )
 
 def load_products():
     with open ('products.json', 'r') as f:
